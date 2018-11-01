66d166465914a44c9ad1c2419114f2bed69a9e13
unknown <felipe.arzola.25@gmail.com>
2018-11-01 03:12:01 -0300

    update

--- Beta codes/Cross-in-tray/Cross-in-tray.py
+++ Beta codes/Cross-in-tray/Cross-in-tray.py
@@ -1,10 +1,35 @@
 import autograd.numpy as np
 from autograd import grad,elementwise_grad
+import pandas as pd
+import timeit
+
+text = []
+r = []
+t = []
 
 def Crosstray(x, y): return -0.0001*(np.absolute(np.sin(x)*np.sin(y)*np.exp(np.absolute(100-(np.sqrt(x**2+y**2)/np.pi))))+1)**0.1
 
+
+
+text.append('Gradient df/dx')
+text.append('Gradient df/dy')
+start = timeit.default_timer()
 dfdx = grad(Crosstray,0)
+r.append(dfdx(1.0,-1.0))
+stop = timeit.default_timer()
+result = (stop-start)
+t.append(result)
+start = timeit.default_timer()
 dfdy = grad(Crosstray,1)
-print "gradient", dfdx(1.0,-1.0)
-print "gradient", dfdy(1.0,-1.0)
+r.append(dfdy(1.0,-1.0))
+stop = timeit.default_timer()
+result = (stop-start)
+t.append(result)
+print " "
+print "Sumary"
+print " "
+data_insertion={'A_Funtion':'Cross in tray','B_Tool':'Autograd','D_Diff':text,'E_Result':r,'F_Time':t}
+df=pd.DataFrame(data_insertion)
+
+print df
 
--- Beta codes/shafferN4/shafferN4.py
+++ Beta codes/shafferN4/shafferN4.py
@@ -1,9 +1,35 @@
 import autograd.numpy as np
 from autograd import grad,elementwise_grad
+import pandas as pd
+import timeit
+
+
+text = []
+r = []
+t = []
+
 
 def shafferN4(x, y): return 0.5+ ((np.cos(np.sin(np.absolute(x**2-y**2)))**2)-0.5/(1+0.001*(x**2+y**2))**2)
 
+
+text.append('Gradient df/dx')
+text.append('Gradient df/dy')
+start = timeit.default_timer()
 dfdx = grad(shafferN4,0)
+r.append(dfdx(2.0,20.0))
+stop = timeit.default_timer()
+result = (stop-start)
+t.append(result)
+start = timeit.default_timer()
 dfdy = grad(shafferN4,1)
-print "gradient", dfdx(2.0,20.0)
-print "gradient", dfdy(2.0,20.0)
+r.append(dfdy(2.0,20.0))
+stop = timeit.default_timer()
+result = (stop-start)
+t.append(result)
+print " "
+print "Sumary"
+print " "
+data_insertion={'A_Funtion':'shafferN4','B_Tool':'Autograd','D_Diff':text,'E_Result':r,'F_Time':t}
+df=pd.DataFrame(data_insertion)
+
+print df
